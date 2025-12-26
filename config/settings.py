99d5def9ad19b34f4c743faad575427c109fe769
Onastasy <95168146+Onastasy@users.noreply.github.com>
2025-12-26 16:12:05 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -73,3 +73,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "profile"
 LOGOUT_REDIRECT_URL = "home"
+
+ALLOWED_HOSTS = ['site-bmk.onrender.com', 'https://site-bmk.onrender.com']
