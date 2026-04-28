41eedd9bd66082cc8c89c66423e5bf0a438c1b25
Onastasy <95168146+Onastasy@users.noreply.github.com>
2026-04-28 12:57:11 +0300

    контекстный процессор для хлебных крошек

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,3 +1,8 @@
 from .models import Banner, ContactInfo
+from .breadcrumbs import get_breadcrumbs
 def site_settings(request):
     return {"banners": Banner.objects.filter(enabled=True)[:3], "contact_info": ContactInfo.objects.first()}
+
+def breadcrumbs(request):
+    """Хлебные крошки для всех страниц"""
+    return {'breadcrumbs': get_breadcrumbs(request)}
