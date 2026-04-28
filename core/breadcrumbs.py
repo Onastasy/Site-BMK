f53d2754b95f248ffe17ea2727d6abae87dfbfc4
Onastasy <95168146+Onastasy@users.noreply.github.com>
2026-04-28 12:53:58 +0300

    модуль хлебных крошек для всех страниц

---
+++ core/breadcrumbs.py
@@ -0,0 +1,38 @@
+def get_breadcrumbs(request):
+    """Формирует хлебные крошки на основе URL"""
+    path = request.path.strip('/').split('/')
+    breadcrumbs = [{'name': 'Главная', 'url': '/'}]
+
+    url_names = {
+        'about': 'О сайте',
+        'about-us': 'О нас',
+        'advantages': 'Преимущества',
+        'team': 'Наша команда',
+        'vacancies': 'Вакансии',
+        'contact': 'Контакты',
+        'sitemap': 'Карта сайта',
+        'search': 'Поиск',
+        'privacy': 'Политика конфиденциальности',
+        'news': 'Новости',
+        'messages': 'Сообщения',
+        'inbox': 'Входящие',
+        'accounts': 'Аккаунт',
+        'login': 'Вход',
+        'register': 'Регистрация',
+        'profile': 'Профиль',
+        'dashboard': 'Личный кабинет',
+        'employees': 'Сотрудники',
+        'admin-panel': 'Админ-панель',
+        'users': 'Пользователи',
+        'groups': 'Группы',
+        'settings': 'Настройки',
+    }
+
+    url = ''
+    for part in path:
+        if part and not part.isdigit():
+            url += f'/{part}'
+            name = url_names.get(part, part.replace('-', ' ').title())
+            breadcrumbs.append({'name': name, 'url': url})
+
+    return breadcrumbs
